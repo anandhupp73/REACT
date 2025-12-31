af4e6ce8b5207392cb6f8435dcea1e697980c7f5
anandhupp73 <anandhakrishnanpp73@gmail.com>
2025-12-31 13:20:40 +0530

    axios

--- queryparams/src/App.jsx
+++ queryparams/src/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css'
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Product from "./Components/Product";
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
 
-function App(){
-  return(
+function App() {
+
+
+  useEffect(() => {
+    fetchFunction()
+  }, [])
+
+  async function fetchFunction() {
+    try {
+     const data =await axios.get("https://dummyjson.com/products")
+     console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/product/:id' element={<Product/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product/:id' element={<Product />} />
       </Routes>
     </Router>
   )
